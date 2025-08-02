6b36698e1d0decfd27ed6810aaac693f4806c46f
RsvsNeeraj0306 <neerajrenkireddy01.gmail.com>
2025-08-02 16:22:58 +0530

    Added Grpc billing with the patient service and created one docker compose file

--- billing-service/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -4,8 +4,10 @@ import billing.BillingRequest;
 import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
 
@@ -14,6 +16,7 @@ public class BillingGrpcService extends BillingServiceImplBase {
     public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
         // Implement your business logic here
 
+        log.info("createBillingAccount request received: {}", request.toString());
         BillingResponse response = BillingResponse.newBuilder()
                 .setAccountId("12345")
                 .setStatus("SUCCESS")
--- patient-service/patient-service/src/main/java/com/pm/patient_service/grpc/BillingServiceGrpcClient.java
+++ patient-service/patient-service/src/main/java/com/pm/patient_service/grpc/BillingServiceGrpcClient.java
@@ -1,5 +1,6 @@
 package com.pm.patient_service.grpc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import io.grpc.ManagedChannel;
@@ -8,29 +9,35 @@ import billing.BillingServiceGrpc;
 import billing.BillingRequest;
 import billing.BillingResponse;
 
+@Slf4j
 @Service
 public class BillingServiceGrpcClient {
 
-    private final BillingServiceGrpc.BillingServiceBlockingStub billingServiceStub;
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
 
     public BillingServiceGrpcClient(
         @Value("${billing.service.address:localhost}") String serverAddress,
         @Value("${billing.service.port:9091}") int serverPort) {
+
+        log.info("Connecting to Billing Service at {}:{}", serverAddress, serverPort);
         
         ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort)
                 .usePlaintext()
                 .build();
-                
-        this.billingServiceStub = BillingServiceGrpc.newBlockingStub(channel);
+
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
     }
 
-    public BillingResponse createBillingAccount(String patientId, String name, String email) {
+    public BillingResponse createBillingAccount(String patientId, String name, String email)
+    {
         BillingRequest request = BillingRequest.newBuilder()
                 .setPatientId(patientId)
                 .setName(name)
                 .setEmail(email)
                 .build();
 
-        return billingServiceStub.createBillingAccount(request);
+        BillingResponse response = blockingStub.createBillingAccount(request);
+        log.info("Received response from Billing Service via Grpc: {}", response);
+        return response;
     }
 }
